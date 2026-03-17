fb214bfc53d17fe9b884bc1fd26ed1748c9a49b3
datvq81 <workmail6401@gmail.com>
2026-03-18 01:17:07 +0700

    socketio server url change

--- my-ts-boardgame/src/App.tsx
+++ my-ts-boardgame/src/App.tsx
@@ -41,7 +41,7 @@ function App() {
       game: customGame,
       board: WrappedBoard, 
       numPlayers: appState === 'editor' ? 1 : numPlayers,
-      multiplayer: appState === 'editor' ? Local() : SocketIO({ server: 'http://localhost:8000' }), 
+      multiplayer: appState === 'editor' ? Local() : SocketIO({ server: 'https://my-game-raxg.onrender.com/' }), 
       debug: false,
     }); 
 
